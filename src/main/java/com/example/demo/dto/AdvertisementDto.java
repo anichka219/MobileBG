e9c713e01800e53ae09a457b30c0ff2b5f3208f7
anichka219 <aieia@abv.bg>
2019-03-05 00:18:22 +0200

    change advertisementDto

---
+++ src/main/java/com/example/demo/dto/AdvertisementDto.java
@@ -0,0 +1,20 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+
+	@Getter
+	@Setter
+	@AllArgsConstructor
+	@EqualsAndHashCode
+public class AdvertisementDto {
+		
+	private long id;
+	private String modification;
+	
+		
+}
+
---
+++ src/main/java/com/example/demo/service/AdvertisementService.java
@@ -0,0 +1,25 @@
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dto.AdvertisementDto;
+import com.example.demo.dto.AdvertisementRepository;
+import com.example.demo.models.Advertisement;
+
+@Service
+public class AdvertisementService {
+	
+	@Autowired
+	private AdvertisementRepository repo;
+	
+	public List<AdvertisementDto> getAllAdvertisements() {
+		return repo.findAll().stream()
+				.map(a->new AdvertisementDto(a.getId(),a.getModification())).collect(Collectors.toList());
+	}
+
+}
