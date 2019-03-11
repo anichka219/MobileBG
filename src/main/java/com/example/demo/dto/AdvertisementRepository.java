d8e47273d8db79b408994065451c2e49ec3c6764
anichka219 <aieia@abv.bg>
2019-03-11 10:40:53 +0200

    changes

--- src/main/java/com/example/demo/dto/AdvertisementRepository.java
+++ src/main/java/com/example/demo/dto/AdvertisementRepository.java
@@ -1,12 +1,11 @@
 package com.example.demo.dto;
-//package com.example.demo.dto;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import com.example.demo.models.Advertisement;
-//
-//@Repository
-//public interface AdvertisementRepository extends JpaRepository<Advertisement,Long>{
-//	
-//}
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.models.Advertisement;
+
+@Repository
+public interface AdvertisementRepository extends JpaRepository<Advertisement,Long>{
+	
+}
